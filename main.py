3fef2cad403eb618ecc96484d7a076c7ab84e0e6
Matheus Lopes Carapina do Nascimento <matheuslcn@id.uff.br>
2022-11-02 19:41:30 -0300

    tudo refeito

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
-from automata import *
-from state import *
 
 def main():
     # abrir arquivo
     file_name = input("Digite o nome do arquivo: ")
     file = open(file_name, "r")
+    lines = file.readlines()
 
-    # criar estados
-    q0 = State("q0")
-    q1 = State("q1")
-    q2 = State("q2")
+    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+    symbols = "+-*/=(),;!><"
+    alphabet = letters + numbers + symbols + " " + "\n"
+    state = 0
+    token = ""
+    reserveds = ["if", "else", "return", "void", "while", "int"]
+    reserved_symbols = ["<=", "<", ">=", ">", "==", "!=", "+", "-", "*", "/", "=", ";", ",", "(", ")", "/*", "*/"]
+    
+    # uma das ideias do roberto
+    for line in lines:
+        tokens = line.split()
+        for token in tokens:
+            if token in reserveds:
+                print("Reservada: ", token)
+            elif token in reserved_symbols:
+                print("Simbolo: ", token)
+            else:
+                continue
+                
 
-    # criar transicoes
-    q0.add_transition("0", q0)
-    q0.add_transition("1", q1)
-
-    q1.add_transition("0", q0)
-    q1.add_transition("1", q2)
-
-    q2.add_transition("0", q0)
-    q2.add_transition("1", q2)
-
-    # criar automato
-    automata = Automata("01", q0, [q2])
-
-    # verificar se o automato aceita o arquivo
-    if automata.accepts(file):
-        print("Aceito")
-    else:
-        print("Rejeitado")
-
-    # fechar o arquivo
-    file.close()
 
 if __name__ == '__main__':
     main()
